a83631f2cad9dba09c5c897d4c927f1542a9e791
michael-nevius <michael.a.nevius@gmail.com>
2022-04-04 11:17:57 -0400

    Robot Class somewhat finished but it is close to being where i want it will work once i finish my other 2 classes

---
+++ robot.py
@@ -0,0 +1,14 @@
+class Robot:
+    def __init__(self, name, Weapon):
+        self.name = "Optimus Prime"
+        self.health = int(100)
+        self.power = int(150)
+        self.weapon = Weapon
+        
+    def attack_dinosaur(self, dinosaur_to_attack):
+        if self.power > 20:
+            print(f'{self.name} attacked{dinosaur_to_attack.type}')
+            self.power -= 20
+            dinosaur_to_attack.health -= self.weapon.attackpower
+            print(f'{self.name} power level is now {self.power}')
+   
