e9971aeaa135456654b6246ec89ebb2abdfd2f8e
michael-nevius <michael.a.nevius@gmail.com>
2022-04-04 15:53:18 -0400

    finally getting closer just missing afew final touches to get it to run correctly

--- battlefield.py
+++ battlefield.py
@@ -23,11 +23,33 @@ class Battlefield:
 
         if user_input == int(1):
             print(f'It looks like {self.dinosaur_name} is going to make the first attack with {self.dinosaur}!')
+            print(f'{self.dinosaur_name} attacks {self.robot_name} for 15 health.')
+            print(f'{self.robot_name} attacks {self.dinosaur_name} for 20 health.')
+            print(f'{self.dinosaur_name} attacks {self.robot_name} for 15 health.')
+            print(f'{self.robot_name} attacks {self.dinosaur_name} for 20 health.')
+            print(f'{self.dinosaur_name} attacks {self.robot_name} for 15 health.')
+            print(f'{self.robot_name} attacks {self.dinosaur_name} for 20 health.')
+            print(f'{self.dinosaur_name} attacks {self.robot_name} for 15 health.')
+            print(f'{self.robot_name} attacks {self.dinosaur_name} for 20 health.')
+            print(f'{self.dinosaur_name} attacks {self.robot_name} for 15 health.')
+            print(f'{self.robot_name} attacks {self.dinosaur_name} for 20 health.')
+            print(f'{self.dinosaur_name} has been defeated by {self.robot_name}!')
             
         
         elif user_input == int(2):
             print(f'It looks like {self.robot_name} is going to make the first attack with {self.robot_weapon}!')
+            print(f'{self.robot_name} attacks {self.dinosaur_name} for 20 health.')
+            print(f'{self.dinosaur_name} attacks {self.robot_name} for 15 health.')
+            print(f'{self.robot_name} attacks {self.dinosaur_name} for 20 health.')
+            print(f'{self.dinosaur_name} attacks {self.robot_name} for 15 health.')
+            print(f'{self.robot_name} attacks {self.dinosaur_name} for 20 health.')
+            print(f'{self.dinosaur_name} attacks {self.robot_name} for 15 health.')
+            print(f'{self.robot_name} attacks {self.dinosaur_name} for 20 health.')
+            print(f'{self.dinosaur_name} attacks {self.robot_name} for 15 health.')
+            print(f'{self.robot_name} attacks {self.dinosaur_name} for 20 health.')
+            print(f'{self.dinosaur_name} has been defeated by {self.robot_name}!')
                 
     
 
-    # def display_winner(self):
+    def display_winner(self):
+        print(f'The winner is {self.robot_name} all praise the Autobots!')
