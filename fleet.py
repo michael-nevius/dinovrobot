54587ba4e24fedd8bb6c7f5cf953b946e34eee8c
michael-nevius <michael.a.nevius@gmail.com>
2022-04-05 08:42:41 -0400

    my fleet file was in the wrong area so didn't get commited but here it is now

---
+++ fleet.py
@@ -0,0 +1,28 @@
+import random
+from robot import Robot
+
+class Fleet:
+    
+    def __init__(self, name):
+        self.name = name
+        self.robots = [Robot('Optimus Prime'), Robot('Bumble Bee'), Robot('Jazz')]
+        self.active_robot = None
+        self.total_fleet_health = 300
+        self.living_robots = []
+
+    def select_active_robot(self):
+        self.calculate_living_robots()
+        if len(self.robots) >= 1:
+            self.active_robot = random.choice(self.robots)
+            print(f'{self.active_robot.name} is going to attack.')
+
+    def calculate_fleet_health(self):
+        self.total_fleet_health = 0
+        for robot in self.robots:
+            self.total_fleet_health += robot.health
+    
+    def calculate_living_robots(self):
+        for robot in self.robots:
+            if robot.health == 0:
+                self.robots.remove(robot)
+                print(f'{robot.name} has been detroyed in battle, please help the auto bots.')
--- robot.py
+++ robot.py
@@ -1,5 +1,6 @@
 from weapon import Weapon
 class Robot:
+    
     def __init__(self, name):
         self.name = name
         self.health = 100
