6d087fb5f64942454830b589028a6564a87bb7f6
michael-nevius <michael.a.nevius@gmail.com>
2022-04-05 08:37:08 -0400

    have my fleet and herd classes done now i'm just trying to figure out why my fleet class isn't getting imported other than that it's just about done

--- battlefield.py
+++ battlefield.py
@@ -1,10 +1,12 @@
-from dinosaur import Dinosaur
-from robot import Robot
+import random
+from fleet import Fleet
+from herd import Herd
+
 
 class Battlefield:
     def __init__(self):
-        self.robot_name = Robot
-        self.dinosaur_name = Dinosaur
+        self.fleet = Fleet('Autobots')
+        self.herd = Herd('Decepticons')
                
 
     def run_game(self):
@@ -13,24 +15,27 @@ class Battlefield:
         self.display_winner()   
 
     def display_welcome(self):
-        print(f'Welcome to Robot vs Dinosaur! Who will be the winner {self.robot_name} or {self.dinosaur_name}?')
+        print(f'Welcome to Robot vs Dinosaur! Who will be the winner {self.fleet} or {self.herd}?')
             
 
     def battle_phase(self):
-        while self.robot_name and self.dinosaur_name:
-            if len(self.robot_name) >= 1:
-                self.robot_name.attack_dinosaur
+        while self.fleet.robots and self.herd.dinosaurs:
+            self.fleet.select_active_robot()
+            if len(self.fleet.robots) >= 1:
+                self.fleet.active_robot.attack(random.choice(self.herd.dinosaurs))
             else:
                 break
-            if len(self.dinosaur_name) >= 1:
-                self.dinosaur_name.attack_robot
+            self.herd.select_active_dionsaur()
+            if len(self.herd.dinosaurs) >= 1:
+                self.herd.active_dinosaur.attack(random.choice(self.fleet.robots))
             else:
                 break
                 
     
 
     def display_winner(self):
-        if len(self.dinosaur_name) == 0:
-            print(f"{self.dinosaur_name} has defeated {self.robot_name}.")
+        if len(self.herd.dinosaurs) == 0:
+            print(f"{self.herd.name} have defeated {self.fleet.name}.")
         else:
-            print(f"{self.robot_name} has defeated {self.dinosaur_name}.")
+            print(f"{self.fleet.name} have defeated {self.herd.name}.")
+
---
+++ herd.py
@@ -0,0 +1,24 @@
+import random
+from dinosaur import Dinosaur
+class Herd:
+    def __init__(self, name):
+        self.name = name
+        self.dinosaurs = [Dinosaur('Grimlock', 35), Dinosaur('Snarl', 25), Dinosaur('Sludge', 30)]
+        self.active_dinosaur = None
+        self.total_herd_health = 300
+
+    def select_active_dionsaur(self):
+        self.calculate_living_dinosaurs()
+        if len(self.dinosaurs) >= 1:
+            self.active_dinosaur = random.choice(self.dinosaurs)
+    
+    def calculate_herd_health(self):
+        self.total_herd_health = 0
+        for dinosaur in self.dinosaurs:
+            self.total_herd_health += dinosaur.health
+
+    def calculate_living_dinosaurs(self):
+        for dinosaur in self.dinosaurs:
+            if dinosaur.health == 0:
+                self.dinosaurs.remove(dinosaur)
+                print(f"{dinosaur.name} has been defeated.")
